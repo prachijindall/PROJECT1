41416ca7089686daaed62097fb0014ac26b1a68b
Prachi Jindal <149607236+prachijindall@users.noreply.github.com>
2024-08-14 22:55:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function changebg()
+{
+    var navbar=document.getElementById('navbar');  /*navbar,scrollvalue=variable*/
+    var scrollValue=window.scrollY;
+    if(scrollValue<150)
+        {
+            navbar.classList.remove('bgcolor');
+        }
+        else{
+            navbar.classList.add('bgcolor');
+        }
+}
+window.addEventListener('scroll',changebg); 
